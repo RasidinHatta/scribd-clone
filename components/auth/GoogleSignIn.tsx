cfd9e51105821a398e79018a1d4b97fcc61ecdec
RasidinHatta <129116066+RasidinHatta@users.noreply.github.com>
2025-04-23 23:48:59 +0800

    add Google Icons

--- components/auth/GoogleSignIn.tsx
+++ components/auth/GoogleSignIn.tsx
@@ -1,6 +1,6 @@
-
 import { signIn } from "@/auth"
 import { Button } from "../ui/button"
+import { FaGoogle } from "react-icons/fa"
 
 export default function GoogleSignIn() {
   return (
@@ -10,9 +10,10 @@ export default function GoogleSignIn() {
         await signIn("google", { redirectTo: "/" })
       }}
     >
-      <Button className="w-full" type="submit">
+      <Button className="w-full flex items-center justify-center" type="submit">
+        <FaGoogle className="mr-2" /> {/* Google logo */}
         Sign In With Google
       </Button>
     </form>
   )
-} 
+}
