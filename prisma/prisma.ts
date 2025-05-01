dd01a1f59cce1644de7c27e96c82cdb6950b4a56
RasidinHatta <129116066+RasidinHatta@users.noreply.github.com>
2025-05-01 12:56:09 +0800

    Update prisma.ts

--- prisma/prisma.ts
+++ prisma/prisma.ts
@@ -1,27 +1,8 @@
 import { PrismaClient } from "@/lib/generated/prisma";
 
-// Create a function that returns a new PrismaClient instance
-const prismaClientSingleton = () => {
-  return new PrismaClient();
-};
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-// Declare global object for Prisma client in non-production environments
-declare const globalThis: {
-  prismaGlobal: PrismaClient | undefined;
-} & typeof global;
+export const db =
+  globalForPrisma.prisma || new PrismaClient();
 
-let db: PrismaClient;
-
-// In development mode, use a single Prisma client instance
-if (process.env.NODE_ENV !== "production") {
-  // Use globalThis to persist the Prisma client in development for hot reloading
-  if (!globalThis.prismaGlobal) {
-    globalThis.prismaGlobal = prismaClientSingleton();
-  }
-  db = globalThis.prismaGlobal;
-} else {
-  // In production, always instantiate a new Prisma client
-  db = prismaClientSingleton();
-}
-
-export default db;
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = db;
