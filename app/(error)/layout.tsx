5ed3bab2fc5edd915556ab9b52ce004ed85001f0
RasidinHatta <129116066+RasidinHatta@users.noreply.github.com>
2025-05-22 13:43:34 +0800

    Update layout.tsx

--- app/(error)/layout.tsx
+++ app/(error)/layout.tsx
@@ -1,10 +1,12 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 
 const ErrorLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="w-full h-full bg-background p-8 rounded-none shadow-none text-center flex flex-col items-center justify-center">
-      {children}
-    </div>
+        <Suspense fallback={<div>Loading...</div>}>
+        {children}
+    </Suspense>
+      </div>
   )
 }
 
